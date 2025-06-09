f53633be9afce1ae0ec30d353859dbd706187c9f
John <jtkline1205@gmail.com>
2025-06-09 19:47:01 -0400

    new endpoints

--- src/routes/wallets.ts
+++ src/routes/wallets.ts
@@ -64,40 +64,28 @@ router.post('/exchange/bills', async (req: Request, res: Response): Promise<any>
             givenBillType = "hundreds"
         }
 
-        console.log('receivedChipType:', receivedChipType);
-        console.log('receivedChipQuantity:', receivedChipQuantity);
-        console.log('givenBillType:', givenBillType);
-
         let givenBillData = await fetchOneWalletsColumn(client, walletId, givenBillType);
-
-        console.log('givenBillData:', givenBillData);
 
         if (givenBillData >= 1) {
             let receivedChipData = await fetchOneWalletsColumn(client, walletId, receivedChipType);
             console.log('receivedChipData:', receivedChipData);
             if (receivedChipData != null) {
                 let newReceivedChipQuantity = receivedChipData + receivedChipQuantity;
-                console.log('newReceivedChipQuantity:', newReceivedChipQuantity);
                 await updateOneWalletsColumn(client, walletId, receivedChipType, newReceivedChipQuantity);
                 let newGivenBillQuantity = givenBillData - 1;
-                console.log('newGivenBillQuantity:', newGivenBillQuantity);
                 await updateOneWalletsColumn(client, walletId, givenBillType, newGivenBillQuantity);
-                console.log('before COMMIT');
                 await client.query('COMMIT');
                 return res.json({ success: true });
             } else {
-                console.log('before ROLLBACK 1');
                 await client.query('ROLLBACK');
                 return res.status(400).json({ success: false, reason: 'Missing column' });
             }
         } else {
-            console.log('before ROLLBACK 2');
             await client.query('ROLLBACK');
             return res.status(400).json({ success: false, reason: 'Insufficient bills' });
         }
 
     } catch (err) {
-        console.error('Exchange bills error:', err);
         res.status(500).send('Something went wrong');
     } finally {
         client.release();
@@ -105,8 +93,6 @@ router.post('/exchange/bills', async (req: Request, res: Response): Promise<any>
 });
 
 router.post('/exchange/chips', async (req: Request, res: Response): Promise<any> => {
-    console.log('Received request for /exchange/chips:', req.body);
-
     const { walletId, denomination } = req.body;
     if (!walletId) return res.status(400).send('Missing walletId');
     const client = await pool.connect();
@@ -143,42 +129,28 @@ router.post('/exchange/chips', async (req: Request, res: Response): Promise<any>
             givenChipType = "chip_hundreds"
         }
 
-        console.log('receivedBillType:', receivedBillType);
-        console.log('givenChipQuantity:', givenChipQuantity);
-        console.log('givenChipType:', givenChipType);
-
         let givenChipData = await fetchOneWalletsColumn(client, walletId, givenChipType);
 
         console.log('givenChipData:', givenChipData);
 
         if (givenChipData >= givenChipQuantity) {
             let receivedBillData = await fetchOneWalletsColumn(client, walletId, receivedBillType);
-            console.log('receivedBillData:', receivedBillData);
             if (receivedBillData != null) {
                 let newReceivedBillQuantity = receivedBillData + 1;
-                console.log('newReceivedBillQuantity:', newReceivedBillQuantity);
                 await updateOneWalletsColumn(client, walletId, receivedBillType, newReceivedBillQuantity);
                 let newGivenChipQuantity = givenChipData - givenChipQuantity;
-                console.log('newGivenChipQuantity:', newGivenChipQuantity);
                 await updateOneWalletsColumn(client, walletId, givenChipType, newGivenChipQuantity);
-                console.log('before COMMIT');
                 await client.query('COMMIT');
                 return res.json({ success: true });
             } else {
-                console.log('before ROLLBACK 1');
                 await client.query('ROLLBACK');
                 return res.status(400).json({ success: false, reason: 'Missing column' });
             }
         } else {
-            console.log('before ROLLBACK 1');
             await client.query('ROLLBACK');
             return res.status(400).json({ success: false, reason: 'Insufficient chips' });
         }
-
-
-
     } catch (err) {
-        console.error('Exchange chips error:', err);
         res.status(500).send('Something went wrong');
     } finally {
         client.release();
@@ -187,5 +159,142 @@ router.post('/exchange/chips', async (req: Request, res: Response): Promise<any>
 
 });
 
+router.post('/break/bills', async (req: Request, res: Response): Promise<any> => {
+    console.log('Received request for /break/bills:', req.body);
+
+    const { walletId, denomination } = req.body;
+    if (!walletId) return res.status(400).send('Missing walletId');
+    const client = await pool.connect();
+
+    try {
+        await client.query('BEGIN');
+
+        let receivedBillType = "ones"
+        let receivedBillQuantity = 5
+        let givenBillType = "fives"
+        if (denomination == 20) {
+            receivedBillType = "fives"
+            givenBillType = "twenties"
+            receivedBillQuantity = 4
+        }
+        if (denomination == 100) {
+            receivedBillType = "twenties"
+            givenBillType = "hundreds"
+            receivedBillQuantity = 5
+        }
+
+        let givenBillData = await fetchOneWalletsColumn(client, walletId, givenBillType);
+
+        if (givenBillData >= 1) {
+            let receivedBillData = await fetchOneWalletsColumn(client, walletId, receivedBillType);
+            if (receivedBillData != null) {
+                let newReceivedBillQuantity = receivedBillData + receivedBillQuantity;
+                await updateOneWalletsColumn(client, walletId, receivedBillType, newReceivedBillQuantity);
+                let newGivenBillQuantity = givenBillData - 1;
+                await updateOneWalletsColumn(client, walletId, givenBillType, newGivenBillQuantity);
+                await client.query('COMMIT');
+                return res.json({ success: true });
+            } else {
+                await client.query('ROLLBACK');
+                return res.status(400).json({ success: false, reason: 'Missing column' });
+            }
+        } else {
+            await client.query('ROLLBACK');
+            return res.status(400).json({ success: false, reason: 'Insufficient bills' });
+        }
+
+
+
+    } catch (err) {
+        console.error('Break bills error:', err);
+        res.status(500).send('Something went wrong');
+    } finally {
+        client.release();
+    }
+
+
+});
+
+router.post('/change/chips', async (req: Request, res: Response): Promise<any> => {
+    console.log('Received request for /change/chips:', req.body);
+
+    const { walletId, givenDenomination, receivedDenomination } = req.body;
+    if (!walletId) return res.status(400).send('Missing walletId');
+    const client = await pool.connect();
+
+    try {
+        await client.query('BEGIN');
+
+        let givenChipQuantity = 1
+        let receivedChipQuantity = 1
+        let givenChipType = "chip_ones"
+        let receivedChipType = "chip_ones"
+        if (givenDenomination == 5 && receivedDenomination == 25) {
+            givenChipQuantity = 5
+            receivedChipQuantity = 1
+            givenChipType = "chip_fives"
+            receivedChipType = "chip_twentyfives"
+        }
+        if (givenDenomination == 25 && receivedDenomination == 5) {
+            givenChipQuantity = 1
+            receivedChipQuantity = 5
+            givenChipType = "chip_twentyfives"
+            receivedChipType = "chip_fives"
+        }
+        if (givenDenomination == 25 && receivedDenomination == 100) {
+            givenChipQuantity = 4
+            receivedChipQuantity = 1
+            givenChipType = "chip_twentyfives"
+            receivedChipType = "chip_hundreds"
+        }
+        if (givenDenomination == 100 && receivedDenomination == 25) {
+            givenChipQuantity = 1
+            receivedChipQuantity = 4
+            givenChipType = "chip_hundreds"
+            receivedChipType = "chip_twentyfives"
+        }
+        if (givenDenomination == 5 && receivedDenomination == 1) {
+            givenChipQuantity = 1
+            receivedChipQuantity = 5
+            givenChipType = "chip_fives"
+            receivedChipType = "chip_ones"
+        }
+        if (givenDenomination == 1 && receivedDenomination == 5) {
+            givenChipQuantity = 5
+            receivedChipQuantity = 1
+            givenChipType = "chip_ones"
+            receivedChipType = "chip_fives"
+        }
+
+
+        let givenChipData = await fetchOneWalletsColumn(client, walletId, givenChipType);
+
+        if (givenChipData >= givenChipQuantity) {
+            let receivedChipData = await fetchOneWalletsColumn(client, walletId, receivedChipType);
+            if (receivedChipData != null) {
+                let newReceivedChipQuantity = receivedChipData + receivedChipQuantity;
+                await updateOneWalletsColumn(client, walletId, receivedChipType, newReceivedChipQuantity);
+                let newGivenChipQuantity = givenChipData - givenChipQuantity;
+                await updateOneWalletsColumn(client, walletId, givenChipType, newGivenChipQuantity);
+                await client.query('COMMIT');
+                return res.json({ success: true });
+            } else {
+                await client.query('ROLLBACK');
+                return res.status(400).json({ success: false, reason: 'Missing column' });
+            }
+        } else {
+            await client.query('ROLLBACK');
+            return res.status(400).json({ success: false, reason: 'Insufficient bills' });
+        }
+
+    } catch (err) {
+        console.error('Change chips error:', err);
+        res.status(500).send('Something went wrong');
+    } finally {
+        client.release();
+    }
+
+
+});
 
 export default router;
