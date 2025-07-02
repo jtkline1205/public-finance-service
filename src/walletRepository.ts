81ab295a80a0a9961f08ed7611aa2d39c737bd9a
John <jtkline1205@gmail.com>
2025-07-02 19:56:49 -0400

    bugfix

--- src/walletRepository.ts
+++ src/walletRepository.ts
@@ -1,11 +1,14 @@
 import { Pool, PoolClient } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const pool = new Pool({
-  host: 'neptune-data.cbceeim2wwnu.us-east-2.rds.amazonaws.com',
-  port: 5432,
-  user: 'postgres',
-  password: 'k0YzA0K1YTzSwSp818Wb',
-  database: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 interface Atm {
